90ead85185992108560f0bc83828bab8fb973722
PavanDNaik <pawannaik396@gmail.com>
2023-07-26 19:25:43 +0530

    Game First commit

---
+++ index.js
@@ -0,0 +1,63 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+    function getRandom(){
+        return Math.floor(Math.random()*(100-1));
+    }
+
+    function highOrLow(v1,v2){
+        if(v1 > v2){
+            return "your answer is Higher";
+        }
+        else {
+            return "your answer is Lower";
+        }
+    }
+    
+    var value=getRandom();
+    var count=0;
+    const submit = document.querySelector("#submit");
+    const guess = document.querySelector("#guessField");
+    const guesses = document.querySelector("#guesses");
+    const lh = document.querySelector("#lowOrHi");
+    submit.addEventListener("click",()=>{
+        if(guess.value == value){
+            guesses.textContent = `You Won the number was ${value}`;
+            lh.textContent="";
+            guess.value="";
+            endGame();
+        }
+        else if( ++count === 1){
+            guesses.textContent = `guesses: ${guess.value}`;
+            lh.textContent=highOrLow(guess.value,value);
+            const temp= document.querySelector("#results");
+            temp.style.border="1px solid black";
+            guess.value="";
+
+        }
+        else if( ++count === 10){
+            guesses.textContent = `You Lost the number was ${value}`;
+            lh.textContent="";
+            guess.value="";
+            endGame();
+        }
+        else{
+            lh.textContent=highOrLow(guess.value,value);
+            guesses.textContent += ` ,${guess.value}`;
+            guess.value="";
+        }
+    });
+    function endGame(){
+     const rerun=document.createElement("button");
+     rerun.innerHTML="One More"
+     const res= document.querySelector("#results");
+     res.appendChild(rerun);
+
+    rerun.addEventListener("click",()=>{
+        guess.textContent="";
+        guesses.textContent="";
+        value=getRandom();
+        count=0;
+        res.removeChild(rerun);
+    });
+    }
+});
